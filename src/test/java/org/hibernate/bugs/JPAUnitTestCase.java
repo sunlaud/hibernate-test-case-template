82dde34e563d2dd849fb76332858cfa93d8466a6
sunlaud <sunlaud@mail.ru>
2018-07-13 00:16:58 +0300

    Initial import

---
+++ src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -0,0 +1,33 @@
+package org.hibernate.bugs;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUnitTestCase {
+
+	private EntityManagerFactory entityManagerFactory;
+
+	@Before
+	public void init() {
+		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
+	}
+
+	@After
+	public void destroy() {
+		entityManagerFactory.close();
+	}
+
+	@Test
+	public void hhh123Test() throws Exception {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		// Do stuff...
+		entityManager.getTransaction().commit();
+		entityManager.close();
+	}
+}
