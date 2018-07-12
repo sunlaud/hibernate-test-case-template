a49d92a8be90c14e5ea62cb8725d2c49b9e70618
sunlaud <sunlaud@mail.ru>
2018-07-13 01:13:51 +0300

    Added simple entities

--- src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,5 +1,7 @@
 package org.hibernate.bugs;
 
+import org.hibernate.bugs.entity.Author;
+import org.hibernate.bugs.entity.Book;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -26,8 +28,28 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+        Author author = new Author("John");
+        Book book = new Book("War and Peace", author);
+
+        entityManager.persist(author);
+        entityManager.persist(book);
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+
+
+        entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+
+        System.out.println("\n =================== Load ===============\n");
+        Book loadedBook = entityManager.find(Book.class, 1L);
+
+        System.out.println("\n =================== Playing ===============\n");
+        System.out.println("author: " + loadedBook.getAuthor().getId());
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
 	}
 }
---
+++ src/test/java/org/hibernate/bugs/entity/Book.java
@@ -0,0 +1,51 @@
+package org.hibernate.bugs.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Author author;
+
+    Book() { }
+
+    public Book(String title, Author author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+}
